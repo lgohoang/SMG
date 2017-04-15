99f819ad3f168d8533bc6fcf9d94a23a752208fb
Hoàng Nguyễn <lgo.hoang@outlook.com>
2017-04-15 12:26:39 +0700

    Add project files.

---
+++ SMG/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMG.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
